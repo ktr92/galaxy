e1ca46949a73aad0becbec36b23aa137c6bc576a
ktr92 <kudashevtim@gmail.com>
2023-02-10 12:47:29 +0300

    desktop

--- src/js/index.js
+++ src/js/index.js
@@ -72,6 +72,18 @@ closeByClickOutside('.mainmenu', '.menubtn')
 var wow = new WOW();
 wow.init();
 
+$(function() {
+  return $(".modal").on("show.bs.modal", function() {
+    var curModal;
+    curModal = this;
+    $(".modal").each(function() {
+      if (this !== curModal) {
+        $(this).modal("hide");
+      }
+    });
+  });
+});
+
 })
 
 
